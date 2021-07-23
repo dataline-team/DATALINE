a7e5ff1e53f8dd1414fb436d67a84ed0ae3c96fd
Vov11x <vov18x@gmail.com>
2021-07-23 22:53:03 +0300

    [lib][Add] boost library

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-
+#include "boost/array.hpp"
 
 int main() 
 {
-    std::cout << "Hello, World!" << std::endl;
+    boost::array<std::string, 2> a {"Hello,", " world!"};  // test boost library
+
+    for (std::string &i: a) {
+        std::cout << i;
+    }
+    
+    std::cout << std::endl;
+
     return 0;
 }
